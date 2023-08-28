53600a57c500314e7fa706615bd1a51626350750
Kornull <u.karnialiuk@gmail.com>
2023-08-28 14:10:16 +0300

    feat: add logo

--- google-clone/app/page.tsx
+++ google-clone/app/page.tsx
@@ -1,10 +1,18 @@
 import Header from '@/components/Header';
+import Image from 'next/image';
 
 export default function Home() {
   return (
     <>
       <Header />
-      <h1>App</h1>
+      <div className="flex flex-col items-center mt-24">
+        <Image
+          src="https://upload.wikimedia.org/wikipedia/commons/5/51/Google.png"
+          width="272"
+          height="100"
+          alt="google-clone"
+        />
+      </div>
     </>
   );
 }
