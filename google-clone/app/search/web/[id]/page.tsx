cdfa309319e392730e583308dd542b8a79954b67
Kornull <u.karnialiuk@gmail.com>
2023-09-03 17:43:08 +0300

    feat: add google api

--- google-clone/app/search/web/[id]/page.tsx
+++ google-clone/app/search/web/[id]/page.tsx
@@ -1,3 +1,6 @@
+import ViewSearchResult from '@/components/UI/ViewSearchResult/ViewSearchResult';
+import { DataItemsType, SearchDataType } from '@/types';
+
 type Props = {
   params: {
     id: string;
@@ -6,13 +9,20 @@ type Props = {
 
 export function generateMetadata({ params: { id } }: Props) {
   return {
-    title: `${id} - Google Clone search`,
-    description: 'Word search Google Clone',
+    title: `${decodeURI(id)} - Google Clone search`,
+    description: `Google-Clone search result - ${decodeURI(id)}`,
   };
 }
 
-const WebSearch = () => {
-  return <div>WebSearch</div>;
+const WebSearch = async ({ params: { id } }: Props) => {
+  const response = await fetch(
+    `https://www.googleapis.com/customsearch/v1?key=${process.env.API_KEY}&cx=${process.env.CONTEXT_KEY}&q=${id}`
+  );
+  const data: SearchDataType = await response.json();
+
+  const items: DataItemsType[] = data.items;
+
+  return <div>{items && <ViewSearchResult data={data} items={items} />}</div>;
 };
 
 export default WebSearch;
