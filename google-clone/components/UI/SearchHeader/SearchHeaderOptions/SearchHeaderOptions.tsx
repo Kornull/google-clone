66ebc52e0cf09564c091f1d696ad870db091433f
Kornull <u.karnialiuk@gmail.com>
2023-09-03 12:14:36 +0300

    feat: refactor search settings btn

--- google-clone/components/UI/SearchHeader/SearchHeaderOptions/SearchHeaderOptions.tsx
+++ google-clone/components/UI/SearchHeader/SearchHeaderOptions/SearchHeaderOptions.tsx
@@ -4,22 +4,43 @@ enum SearchSettings {
   WEB = 'web',
   IMAGE = 'image',
 }
+enum SearchSettingsName {
+  WEB = 'All',
+  IMAGE = 'Image',
+}
 
-import { usePathname } from 'next/navigation';
+import { usePathname, useRouter } from 'next/navigation';
 import { useState } from 'react';
+import { SearchHeaderButton } from './SearchHeaderButton/SearchHeaderButton';
 
 const SearchHeaderOptions = () => {
   const [search, setSearch] = useState<string>(SearchSettings.WEB);
+  const router = useRouter();
   const pathname = usePathname();
+
+  const selectTab = (path: string) => {
+    const searchArray = pathname.split('/');
+    const searchData = searchArray[searchArray.length - 1];
+
+    setSearch(path);
+    router.push(`/search/${path}/${searchData}?item=${searchData}`);
+  };
+
   return (
     <div className=" flex items-start px-6 py-3 border-b-2 border-gray-200">
       <div className=" flex items-center gap-1">
-        <button className={`page__search-btn ${search === SearchSettings.WEB ? 'active' : ''}`}>
-          All
-        </button>
-        <button className={`page__search-btn ${search === SearchSettings.WEB ? '' : 'active'}`}>
-          Image
-        </button>
+        <SearchHeaderButton
+          style={`page__search-btn ${search === SearchSettings.WEB ? 'active' : ''}`}
+          onClick={selectTab}
+          name={SearchSettingsName.WEB}
+          data={SearchSettings.WEB}
+        />
+        <SearchHeaderButton
+          style={`page__search-btn ${search === SearchSettings.WEB ? '' : 'active'}`}
+          onClick={selectTab}
+          name={SearchSettingsName.IMAGE}
+          data={SearchSettings.IMAGE}
+        />
       </div>
     </div>
   );
